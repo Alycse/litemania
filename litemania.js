aab5e3b0e0912f2660b23e7eb21213081e983283
Alycse <Alycse@gmail.com>
2019-10-05 11:20:52 +0800

    Create song class

--- litemania.js
+++ litemania.js
@@ -7,48 +7,107 @@ function lmDeclare(){
     }
     window.config = {
         despawnPosY: 650,
-        missPosY: 600
+        missPosY: 600,
+        barSpawnPosY: -550,
+        lane0PosX: -363,
+        lane1PosX: -120,
+        lane2PosX: 123,
+        lane3PosX: 366
     }
 }
 
+var currentSong = null;
+
 function lmInit(){
-    track = new worldObject("Track", {id: 0, images: {default: "/assets/track.png"}}, {x: 0, y: 0}, 0, 1.5, 1, "track");
+    track = new WorldObject("Track", {id: 0, images: {default: "/assets/track.png"}}, {x: 0, y: 0}, 0, 1.5, 1, "track");
     track.spawn();
 
-    spawnBar(0);
-    spawnBar(1);
-    spawnBar(2);
-    spawnBar(3);
+    var song = new Song();
+    song.addBar(0, 0);
+    song.addBar(1, 1);
+    song.addBar(2, 3);
+    song.addBar(3, 2);
+
+    startSong(song);
+}
+
+function startSong(song){
+    currentSong = song;
+    currentSong.reset();
 }
 
 function lmUpdate(){
+    if(currentSong != null){
+        currentSong.currentTime += deltaTime;
+        if(currentSong.currentBeatmapPosition < currentSong.beatmap.length){
+            if(currentSong.currentTime >= currentSong.beatmap[currentSong.currentBeatmapPosition].time){
+                spawnBar(currentSong.beatmap[currentSong.currentBeatmapPosition].lane);
+                currentSong.currentBeatmapPosition++;
+            }
+        }else{
+            console.log("Song finished!");
+            currentSong = null;
+        }
+    }
+
     for(var i in bars){
-        bars[i].position.y += deltaTime * settings.speed;
-        if(bars[i].position.y > config.despawnPosY){
+        bars[i].body.position.y += deltaTime * settings.speed;
+        bars[i].fadeIn();
+        if(bars[i].body.position.y > config.despawnPosY){
             bars[i].despawn();
-        } else if(bars[i].position.y > config.missPosY){
-            missBar(bars[i]);
+        } else if(bars[i].body.position.y > config.missPosY){
+            bars[i].missFadeOut();
         }
     }
 }
 
-function spawnBar(position){
-    if(position == 0){
-        var bar = new worldObject("Bar", {id: 0, images: {default: "/assets/gray-bar.png"}}, {x: -363, y: -300}, 0, 1.45, 0, "bar");
-    }else if(position == 1){
-        var bar = new worldObject("Bar", {id: 0, images: {default: "/assets/pink-bar.png"}}, {x: -120, y: -100}, 0, 1.45, 0, "bar");
-    }else if(position == 2){
-        var bar = new worldObject("Bar", {id: 0, images: {default: "/assets/pink-bar.png"}}, {x: 123, y: 100}, 0, 1.45, 0, "bar");
-    }else{
-        var bar = new worldObject("Bar", {id: 0, images: {default: "/assets/gray-bar.png"}}, {x: 366, y: 300}, 0, 1.45, 0, "bar");
-    }
-    bar.alpha = 1;
+function spawnBar(lane){
+    var bar = new Bar(lane);
     bars.push(bar);
-    bar.spawn();
 }
 
-function missBar(bar){
-    bar.width += deltaTime * 400;
-    bar.height += deltaTime * 400;
-    bar.alpha = Math.max(bar.alpha - (deltaTime * 10), 0);
+class Bar {
+    constructor(lane){
+        if(lane == 0){
+            this.body = new WorldObject("Bar", {id: 0, images: {default: "/assets/gray-bar.png"}}, {x: config.lane0PosX, y: config.barSpawnPosY}, 0, 1.45, 0, "bar");
+        }else if(lane == 1){
+            this.body = new WorldObject("Bar", {id: 0, images: {default: "/assets/pink-bar.png"}}, {x: config.lane1PosX, y: config.barSpawnPosY}, 0, 1.45, 0, "bar");
+        }else if(lane == 2){
+            this.body = new WorldObject("Bar", {id: 0, images: {default: "/assets/pink-bar.png"}}, {x: config.lane2PosX, y: config.barSpawnPosY}, 0, 1.45, 0, "bar");
+        }else{
+            this.body = new WorldObject("Bar", {id: 0, images: {default: "/assets/gray-bar.png"}}, {x: config.lane3PosX, y: config.barSpawnPosY}, 0, 1.45, 0, "bar");
+        }
+        this.body.alpha = 0;
+        this.body.spawn();
+    }
+
+    fadeIn = function(){
+        this.body.alpha = Math.min(this.body.alpha + (deltaTime * 3), 1);
+    }
+
+    missFadeOut = function(){
+        this.body.width += deltaTime * 400;
+        this.body.height += deltaTime * 400;
+        this.body.alpha = Math.max(this.body.alpha - (deltaTime * 10), 0);
+    }
+
+    despawn = function(){
+        this.body.despawn();
+    }
+}
+
+class Song {
+    constructor(){
+        this.barCount = 0;
+        this.beatmap = new Array();
+        this.currentTime = 0;
+        this.currentBeatmapPosition = 0;
+    }
+    addBar = function(time, lane){
+        this.beatmap[this.barCount++] = {time: time, lane: lane};
+    }
+    reset = function(){
+        this.currentTime = 0;
+        this.currentBeatmapPosition = 0;
+    }
 }
